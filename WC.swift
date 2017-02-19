9ae088e17189bc64360c2d457c133e5903badf8f
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-19 08:45:51 +0100

    Update WC.swift

--- WC.swift
+++ WC.swift
@@ -1,10 +1,71 @@
+import Foundation
+
+struct User {
+  
+  let uid: String
+  let email: String
+  
+  init(authData: FIRUser) {
+    uid = authData.uid
+    email = authData.email!
+  }
+  
+  init(uid: String, email: String) {
+    self.uid = uid
+    self.email = email
+  }
+  
+}
+
+
+
+import Foundation
+
+struct GroceryItem {
+  
+  let key: String
+  let name: String
+  let addedByUser: String
+  let ref: FIRDatabaseReference?
+  var completed: Bool
+  
+  init(name: String, addedByUser: String, completed: Bool, key: String = "") {
+    self.key = key
+    self.name = name
+    self.addedByUser = addedByUser
+    self.completed = completed
+    self.ref = nil
+  }
+  
+  init(snapshot: FIRDataSnapshot) {
+    key = snapshot.key
+    let snapshotValue = snapshot.value as! [String: AnyObject]
+    name = snapshotValue["name"] as! String
+    addedByUser = snapshotValue["addedByUser"] as! String
+    completed = snapshotValue["completed"] as! Bool
+    ref = snapshot.ref
+  }
+  
+  func toAnyObject() -> Any {
+    return [
+      "name": name,
+      "addedByUser": addedByUser,
+      "completed": completed
+    ]
+  }
+  
+}
+
+
+
+
+
+
 
 import UIKit
 
 class LoginViewController: UIViewController {
-  
-  
-  
+ 
   // MARK: Outlets
   @IBOutlet weak var textFieldLoginEmail: UITextField!
   @IBOutlet weak var textFieldLoginPassword: UITextField!
