0c51559a92bedba4d0fb99582fb83305ff83979e
Filip Vabrousek <fvabrousek@yahoo.com>
2017-02-19 08:57:01 +0100

    Update WC.swift

--- WC.swift
+++ WC.swift
@@ -65,42 +65,53 @@ struct GroceryItem {
 import UIKit
 
 class LoginViewController: UIViewController {
- 
+
+  // MARK: Constants
+  let loginToList = "LoginToList"
+  
   // MARK: Outlets
   @IBOutlet weak var textFieldLoginEmail: UITextField!
   @IBOutlet weak var textFieldLoginPassword: UITextField!
   
-  // MARK: Actions
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-  }
-  
-  
-  /*--------------------------------------------LOGIN----------------------------------------------*/
-  @IBAction func loginDidTouch(_ sender: Any) {
-    FIRAuth.auth()?.signIn(withEmail: textFieldLoginEmail.text!, password: textFieldLoginPassword.text!)
-  }
-  
-  
- /*---------------------------------------------SIGN UP--------------------------------------*/
-  @IBAction func signUpDidTouch(_ sender: AnyObject) {
-    let alert = UIAlertController(title: "Register", message: "Register", preferredStyle: .alert)
     
-    let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+    FIRAuth.auth()!.addStateDidChangeListener() { auth, user in
+      if user != nil {
+        self.performSegue(withIdentifier: self.loginToList, sender: nil)
+      }
+    }
+  }
+  
+  /*--------------------------------------------LOGIN---------correct-------------------------------------*/
+  @IBAction func loginDidTouch(_ sender: AnyObject) {
+    FIRAuth.auth()!.signIn(withEmail: textFieldLoginEmail.text!,
+                           password: textFieldLoginPassword.text!)
+  }
+
+  
+  /*---------------------------------------------SIGN UP--------------------------------------*/
+  @IBAction func signUpDidTouch(_ sender: AnyObject) {
+    let alert = UIAlertController(title: "Register",
+                                  message: "Register",
+                                  preferredStyle: .alert)
+    
+    let saveAction = UIAlertAction(title: "Save",
+      style: .default) { action in
+        let emailField = alert.textFields![0] 
+        let passwordField = alert.textFields![1] 
       
-      let emailField = alert.textFields![0]
-      let passwordField = alert.textFields![1]
-      
-      FIRAuth.auth()?.createUser(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
-        
-        if error != nil {
-          FIRAuth.auth()?.signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
+        FIRAuth.auth()!.createUser(withEmail: emailField.text!,
+                                   password: passwordField.text!) { user, error in
+          if error == nil {
+            FIRAuth.auth()!.signIn(withEmail: self.textFieldLoginEmail.text!,
+                                   password: self.textFieldLoginPassword.text!)
+          }
         }
-      })
     }
     
-    let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+    let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .default)
     
     alert.addTextField { textEmail in
       textEmail.placeholder = "Enter your email"
@@ -116,7 +127,7 @@ class LoginViewController: UIViewController {
     
     present(alert, animated: true, completion: nil)
   }
-  
+
 }
 
 
@@ -146,9 +157,10 @@ extension LoginViewController: UITextFieldDelegate {
 
 
 
-/*
-  Copyright (c) 2015 Razeware LLC
- */
+
+
+
+
 
 import UIKit
 
@@ -159,36 +171,45 @@ class GroceryListTableViewController: UITableViewController {
   
   // MARK: Properties 
   var items: [GroceryItem] = []
-  var user: User!
-  var userCountBarButtonItem: UIBarButtonItem!
   let ref = FIRDatabase.database().reference(withPath: "grocery-items")
   let usersRef = FIRDatabase.database().reference(withPath: "online")
+  var user: User!
+  var userCountBarButtonItem: UIBarButtonItem!
   
-  
+
   /*-------------------------------VIEW DID LOAD---------------------------
    1 - order database items by "completed"
    2 - append databse items to newItems
    3 - append items to newItems
    */
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     tableView.allowsMultipleSelectionDuringEditing = false
     
-    userCountBarButtonItem = UIBarButtonItem(title: "1", style: .plain, target: self, action: #selector(self.userCountButtonDidTouch))
+    userCountBarButtonItem = UIBarButtonItem(title: "1",
+                                             style: .plain,
+                                             target: self,
+                                             action: #selector(userCountButtonDidTouch))
     userCountBarButtonItem.tintColor = UIColor.white
     navigationItem.leftBarButtonItem = userCountBarButtonItem
     
+    usersRef.observe(.value, with: { snapshot in
+      if snapshot.exists() {
+        self.userCountBarButtonItem?.title = snapshot.childrenCount.description
+      } else {
+        self.userCountBarButtonItem?.title = "0"
+      }
+    })
     
     //1
     ref.queryOrdered(byChild: "completed").observe(.value, with: { snapshot in
       var newItems: [GroceryItem] = []
       
       //2
-      for item in snapshot.children{
-      let groceryItem = GroceryItem(snapshot: item as! FIRDataSnapshot)
-      newItems.append(groceryItem)
+      for item in snapshot.children {
+        let groceryItem = GroceryItem(snapshot: item as! FIRDataSnapshot)
+        newItems.append(groceryItem)
       }
       
       //3
@@ -196,35 +217,17 @@ class GroceryListTableViewController: UITableViewController {
       self.tableView.reloadData()
     })
     
-    
-  /*
-  ???
-     
-  
-    
-  let currentUserRef = self.usersRef.child(self.user.uid)
-  currentUserRef.setValue(self.user.email)
-  currentUserRef.onDisconnectRemoveValue()
-    */
-    
-    usersRef.observe(.value, with: { (snapshot) in
-      if snapshot.exists(){
-      self.userCountBarButtonItem?.title = snapshot.childrenCount.description
-      } else {
-      self.userCountBarButtonItem?.title = "0"
-      }
-    })
-    
     FIRAuth.auth()!.addStateDidChangeListener { auth, user in
       guard let user = user else { return }
       self.user = User(authData: user)
+      let currentUserRef = self.usersRef.child(self.user.uid)
+      currentUserRef.setValue(self.user.email)
+      currentUserRef.onDisconnectRemoveValue()
     }
-  
   }
   
   
   /*-------------------------------TABLEVIEW METHODS--------------------------*/
-  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return items.count
   }
@@ -235,6 +238,7 @@ class GroceryListTableViewController: UITableViewController {
     
     cell.textLabel?.text = groceryItem.name
     cell.detailTextLabel?.text = groceryItem.addedByUser
+    
     toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
     
     return cell
@@ -246,19 +250,20 @@ class GroceryListTableViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
-     let groceryItem = items[indexPath.row]
-    groceryItem.ref?.removeValue()
+      let groceryItem = items[indexPath.row]
+      groceryItem.ref?.removeValue()
     }
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     guard let cell = tableView.cellForRow(at: indexPath) else { return }
-    var groceryItem = items[indexPath.row]
+    let groceryItem = items[indexPath.row]
     let toggledCompletion = !groceryItem.completed
     toggleCellCheckbox(cell, isCompleted: toggledCompletion)
-    groceryItem.ref?.updateChildValues(["completed" : toggledCompletion])
+    groceryItem.ref?.updateChildValues([
+      "completed": toggledCompletion
+    ])
   }
-  
   
   
   
@@ -275,31 +280,44 @@ class GroceryListTableViewController: UITableViewController {
     }
   }
   
-  /*-------------------------------ADD ITEM--------------------------*/
-  
+  /*-------------------------------------------------ADD ITEM------------------------------------------------*/
   @IBAction func addButtonDidTouch(_ sender: AnyObject) {
-    let alert = UIAlertController(title: "Grocery Item", message: "Add an Item", preferredStyle: .alert)
+    let alert = UIAlertController(title: "Grocery Item",
+                                  message: "Add an Item",
+                                  preferredStyle: .alert)
     
-    let saveAction = UIAlertAction(title: "Save", style: .default) { action in
-      guard let textField = alert.textFields?.first,
-        let text = textField.text else { return }
-      
-      
-    let groceryItem = GroceryItem(name: text, addedByUser: self.user.email, completed: false)
-    let groceryItemRef = self.ref.child(text.lowercased())
-    groceryItemRef.setValue(groceryItem.toAnyObject)
+    let saveAction = UIAlertAction(title: "Save",
+                                   style: .default) { _ in
+                                    // 1
+                                    guard let textField = alert.textFields?.first,
+                                      let text = textField.text else { return }
+
+                                    // 2
+                                    let groceryItem = GroceryItem(name: text,
+                                                                  addedByUser: self.user.email,
+                                                                  completed: false)
+                                    // 3
+                                    let groceryItemRef = self.ref.child(text.lowercased())
+
+                                    // 4
+                                    groceryItemRef.setValue(groceryItem.toAnyObject())
+    }
     
-    let cancelAction = UIAlertAction(title: "Cancel",style: .default)
+    let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .default)
+    
     alert.addTextField()
+    
+    alert.addAction(saveAction)
     alert.addAction(cancelAction)
-    self.present(alert, animated: true, completion: nil)
-  }
-   alert.addAction(saveAction)
+    
+    present(alert, animated: true, completion: nil)
   }
   
   func userCountButtonDidTouch() {
     performSegue(withIdentifier: listToUsers, sender: nil)
   }
+  
 }
 
 
@@ -309,55 +327,53 @@ class GroceryListTableViewController: UITableViewController {
 
 
 
-/*
- Copyright (c) 2015 Razeware LLC
-*/
+
+
+
+
+
 
 import UIKit
 
 class OnlineUsersTableViewController: UITableViewController {
-  
+
   // MARK: Constants
   let userCell = "UserCell"
+  let usersRef = FIRDatabase.database().reference(withPath: "online")
   
   // MARK: Properties
   var currentUsers: [String] = []
-  let usersRef = FIRDatabase.database().reference(withPath: "online")
   
-  // MARK: UIViewController Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    /*-------------------------------USERS REF OBSERVE---------------1----------*/
     
+    /*-------------------------------USERS REF OBSERVE---------------1----------*/
     usersRef.observe(.childAdded, with: { snap in
       guard let email = snap.value as? String else { return }
       self.currentUsers.append(email)
-      
       let row = self.currentUsers.count - 1
       let indexPath = IndexPath(row: row, section: 0)
       self.tableView.insertRows(at: [indexPath], with: .top)
     })
     
-    usersRef.observe(.childRemoved, with: { (snap) in
+    
+    /*-------------------------------USERS REF OBSERVE---------------2----------*/
+    usersRef.observe(.childRemoved, with: { snap in
       guard let emailToFind = snap.value as? String else { return }
-      
-      for (index, email) in self.currentUsers.enumerated(){
-        if email == emailToFind{
-        let indexPath = IndexPath(row: index, section: 0)
-        self.currentUsers.remove(at: index)
-        self.tableView.deleteRows(at: [indexPath], with: .fade)
+      for (index, email) in self.currentUsers.enumerated() {
+        if email == emailToFind {
+          let indexPath = IndexPath(row: index, section: 0)
+          self.currentUsers.remove(at: index)
+          self.tableView.deleteRows(at: [indexPath], with: .fade)
         }
-      
       }
     })
+    
   }
   
-  /*-------------------------------USERS REF OBSERVE---------------2----------*/
-
-  
-  // MARK: UITableView Delegate methods
-  
+  /*-------------------------------TABLEVIEW METHODS--------------------------*/
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return currentUsers.count
   }
@@ -369,14 +385,16 @@ class OnlineUsersTableViewController: UITableViewController {
     return cell
   }
   
-  // MARK: Actions
-  
+  /*-----------------------------------SIGN OUT--------------------------------*/
   @IBAction func signoutButtonPressed(_ sender: AnyObject) {
-    dismiss(animated: true, completion: nil)
+    do {
+      try FIRAuth.auth()!.signOut()
+      dismiss(animated: true, completion: nil)
+    } catch {
+      
+    }
   }
   
 }
-
-
 
 
