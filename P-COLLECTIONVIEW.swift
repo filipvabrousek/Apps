5b2ab82f77419bd3fd5a958def28a1efebd79632
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-07-23 23:32:34 +0200

    Update P-COLLECTIONVIEW.swift

--- P-COLLECTIONVIEW.swift
+++ P-COLLECTIONVIEW.swift
@@ -1,38 +1,34 @@
+
+import UIKit
+import CoreGraphics
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
- 
     
-    var slider: UISlider = UISlider()
     
-    @IBOutlet var imgView: UIImageView!
-    
-    var cv:UICollectionView!
+    var cv:UICollectionView! // NO var vc = UICollectionView() -> would be ERROR  !!!!
     
     let arr = ["Filip", "Sara", "Kaja", "Maja", "Paja", "Aja"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
         layout.itemSize = CGSize(width: 60, height: 60)
-        
+        layout.scrollDirection = .horizontal
         
         let rect = CGRect(x: 0, y: 200, width: self.view.bounds.width, height: 90)
         cv = UICollectionView(frame: rect, collectionViewLayout: layout)
         cv.delegate = self
         cv.dataSource = self
         cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
-       // cv.showsHorizontalScrollIndicator = true
         cv.backgroundColor = UIColor.gray
-   
+        
         view.addSubview(cv)
         
-       
-    }
-  
-    
  
-
+    }
+    
+  
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return arr.count
@@ -43,4 +39,3 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cell.backgroundColor = UIColor.orange
         return cell
     }
-    
