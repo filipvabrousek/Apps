1a960cb9afcc0f12cbcf078a3a54c55b61a0ad92
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-08-24 23:57:49 +0200

    Create Pin-original.swift

---
+++ Pin-original.swift
@@ -0,0 +1,44 @@
+
+extension UIView {
+    
+    func fillSuperview(){
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+    
+    func anchorSize(to: UIView){
+        widthAnchor.constraint(equalTo: to.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: to.heightAnchor).isActive = true
+    }
+    
+    
+    // https://www.youtube.com/watch?v=iqpAP7s3b-8
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+ 
+}
