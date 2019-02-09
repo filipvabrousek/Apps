9082d7fbb37f2b23e1f8982ae2daab699dbf38f5
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-02-09 21:25:56 +0000

    Add files via upload

---
+++ pedometer.swift
@@ -0,0 +1,11 @@
+ @objc func pedo(sender: UIButton!){
+    
+    if CMPedometer.isStepCountingAvailable(){
+               
+        PM.startUpdates(from: Date()) { (data, err) in
+            if let data = data {
+                print("Number of steps \(data.numberOfSteps)")
+            } 
+        }
+    }
+ }
